ceae9faac6e84c8e8eacad8bd6f2de1be62ec693
Prasanth <Prasanth@DESKTOP-LVSF1UE>
2021-12-03 08:04:38 -0600

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author srilaxmi
+ * this my first ever java program
+ *program to print "Helloworld"
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		 System.out.println("Hello, World!");
+		
+
+	}
+
+}
